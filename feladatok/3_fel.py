39fc4600b3502d9e055d8018ea6326c2b4140ad2
edom <molonar@inf.u-szeged.hu>
2021-11-23 20:02:07 +0100

    3.fel done

---
+++ feladatok/3_fel.py
@@ -0,0 +1,25 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+G = nx.DiGraph()
+G.add_edges_from(
+    [('1', '4'), ('1', '6'), ('2', '3'), ('2', '4'), ('3', '1'),
+     ('3', '2'), ('3', '4'), ('3', '5'), ('3', '6'), ('5', '1'),
+     ('5', '3'), ('6', '1'), ('6', '4'), ('6', '5')])
+
+ket_iranyu_graf = [('1', '4'), ('2', '3'), ('5', '3'), ('1', '6'), ('3', '2'), ('3', '5'), ('6', '1')]
+
+iranyitott_graf = [edge for edge in G.edges() if edge not in ket_iranyu_graf]
+
+pos = nx.spring_layout(G)
+nx.draw_networkx_nodes(G, pos, cmap=plt.get_cmap('jet'),
+                       node_size=500)
+nx.draw_networkx_labels(G, pos)
+
+nx.draw_networkx_edges(G, pos,
+                       edgelist=iranyitott_graf,
+                       edge_color='r', arrows=True)
+nx.draw_networkx_edges(G, pos,
+                       edgelist=ket_iranyu_graf,
+                       arrows=False)
+plt.show()
